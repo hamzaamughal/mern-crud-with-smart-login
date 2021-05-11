ee91861402cfb45f03ca2cdba748ed4f9bc70677
hamzamughal <mughalh473@gmail.com>
2021-05-11 23:08:27 +0500

    create post from UI(react) completed

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@
 
 function App() {
   return (
-    <h1>hello</h1>
+    <div className="container p-5">MERN CRUD</div>
   );
 }
 
---
+++ client/src/Create.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const Create = () => {
+    //state
+    const [state, setState] = useState({
+        title: '',
+        content: '',
+        user: ''
+    })
+
+    //destructure values from state
+    const { title, content, user } = state
+
+    //onChange event handler
+    const handleChange = (name) => (event) => {
+        console.log('name', name, 'event', event.target.value);
+        setState({ ...state, [name]: event.target.value })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        axios.post(`${process.env.REACT_APP_API}/post`, { title, content, user })
+            .then(response => {
+                //empty state
+                setState({ ...state, title: '', content: '', user: '' })
+                //show success alert
+                alert(`Post titled: ${response.data.title} is created`)
+            })
+            .catch(error => {
+                console.log(error.response);
+                alert(error.response.data.error)
+            })
+    }
+
+    return (
+        <div className="container p-5">
+            <h1>CREATE POST</h1>
+            <br />
+            <form onSubmit={handleSubmit} action="">
+                <div className="form-group m-3">
+                    <label htmlFor="title" className="text-muted p-2">Title</label>
+                    <input onChange={handleChange('title')} value={title} id="title" type="text" className="form-control" placeholder="Post title" required />
+                </div>
+                <div className="form-group m-3">
+                    <label htmlFor="content" className="text-muted p-2">Content</label>
+                    <textarea onChange={handleChange('content')} value={content} id="content" type="text" className="form-control" placeholder="Write something.." required />
+                </div>
+                <div className="form-group m-3">
+                    <label htmlFor="user" className="text-muted p-2">User</label>
+                    <input onChange={handleChange('user')} value={user} id="user" type="text" className="form-control" placeholder="Your name" required />
+                </div>
+                <div className="m-3 p-2">
+                    <button className="btn btn-primary">Create</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Create
